36518ae7383a5380067426d7586c19ba13bd4401
Vivien Riehl <vivienriehl.alsace@gmail.com>
2022-01-30 17:33:11 +0100

    Ajout map, point d'intérêt et itinéraire

--- Developpement/Mobile/mobile_project/App.js
+++ Developpement/Mobile/mobile_project/App.js
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
+
+  const GOOGLE_MAPS_APIKEY = 'AIzaSyAJVvWk_VD4fFSTgIbKZn4mKbudKeQXEII';
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container}>
+      <MapView style={styles.map}>
+        <Marker title="Cathédrale de Strasbourg" coordinate={{ latitude: 48.5818717456352, longitude: 7.75104124324297 }}>
+        </Marker>
+        <Marker title="Musée du Pays de Hanau" coordinate={{ latitude: 48.824838302922835, longitude: 7.482993317810144 }}>
+        </Marker>
+        <Marker title="Hotel 🛌🏻" description="C'est le meilleur hotel de Paris" coordinate={{ latitude: 48.86809588857603, longitude: 2.328886469618505 }} pinColor="blue">
+        </Marker>
+        <MapViewDirections
+          origin={{ latitude: 48.5818717456352, longitude: 7.75104124324297 }}
+          destination={{ latitude: 48.824838302922835, longitude: 7.482993317810144 }}
+          strokeWidth={3}
+          strokeColor='red'
+          apikey={GOOGLE_MAPS_APIKEY}
+          onReady={result => {
+            console.log(`Distance: ${result.distance} km`)
+            console.log(`Duration: ${result.duration} min.`)
+          }} />
+      </MapView>
+    </SafeAreaProvider>
   );
 }
 
@@ -16,5 +38,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });

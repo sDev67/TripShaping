e2e8edd519f19cf222d54cd4d667ced9e201474b
Benjamin Gallier <bgallier@unistra.fr>
2022-03-03 16:31:12 +0100

    Fin premier sprint

--- Developpement/Web/web_project/src/TodoList/LabelForm.js
+++ Developpement/Web/web_project/src/TodoList/LabelForm.js
@@ -1,55 +1,47 @@
-import { Stack } from '@mui/material';
-import {TextField, IconButton, Typography} from "@mui/material";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import {useEffect, useState} from 'react'
+import { Stack } from "@mui/material";
+import { TextField, Button, Typography } from "@mui/material";
+import DoneRounded from "@mui/icons-material/DoneRounded";
+import { useEffect, useState } from "react";
 
-const LabelForm = ({label}) =>
-{
-    const [currentTitle, setCurrentTitle] = useState('');
+const LabelForm = ( {addLabel} ) => {
+  const [currentTitle, setCurrentTitle] = useState("");
 
-    useEffect(() => {
-
-
-        if(label.title !== undefined)
-        {
-            setCurrentTitle(label.title);
-        }
-
-    }, [label])
+  //   useEffect(() => {
+  //     if (label.title !== undefined) {
+  //       setCurrentTitle(label.title);
+  //     }
+  //   }, [label]);
 
 
-    const handleSubmit = () => 
-    {
 
-    }
-
-    return(
-        <>
-            <Stack direction='column'
-            alignItems='flex-start'
-            style={{ height: "100%", width: "90%", margin:'20px' }}>
-              
-            {label.title === undefined ^ label.title === 'Title'  ? 
-                <Typography variant='h3'>Create a Task</Typography> :
-                <Typography variant='h3'>Modify {currentTitle}</Typography>
-            }
-                
-                <TextField
-                        required
-                        id="secondary-required"
-                        label="Required"
-                        onChange={(e) => setCurrentTitle(e.target.value)}
-                      
-                        variant="standard"
-                        style={{marginRight:11}} />
-
-                <IconButton aria-label="Add" onClick={handleSubmit}>
-                    <AddCircleIcon sx={{fontSize:'35px'}} color='secondary' />
-                </IconButton>     
-
-            </Stack>
-        </>
-    )
-}
+  return (
+    <>
+      <Stack
+        direction="column"
+        alignItems="flex-start"
+        style={{ height: "100%", margin: "20px" }}
+      >
+        <Typography variant="h3" marginY={2}>
+          Créer un label
+        </Typography>
+        <Stack direction="row" width="100%" spacing={1}>
+          <TextField
+            id="secondary-required"
+            label="Nom"
+            onChange={(e) => setCurrentTitle(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<DoneRounded />}
+            onClick={addLabel(currentTitle)}
+          >
+            Enregistrer
+          </Button>
+        </Stack>
+      </Stack>
+    </>
+  );
+};
 
 export default LabelForm;
--- Developpement/Web/web_project/src/TodoList/TasksItemGrid.js
+++ Developpement/Web/web_project/src/TodoList/TasksItemGrid.js
@@ -1,72 +1,138 @@
-import '../Styles/ButtonStyles.css';
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import {Stack,List,ListItemButton, Box,Divider,Chip,Grid,Typography} from "@mui/material";
-import ClassOutlinedIcon from '@mui/icons-material/ClassOutlined';
-import '../App.css';
-import IconButton from '@mui/material/IconButton';
+import "../Styles/ButtonStyles.css";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import {
+  Stack,
+  List,
+  ListItemButton,
+  Box,
+  Divider,
+  Chip,
+  Grid,
+  Typography,
+} from "@mui/material";
+import ClassOutlinedIcon from "@mui/icons-material/ClassOutlined";
+import "../App.css";
+import IconButton from "@mui/material/IconButton";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
-const TasksItemGrid = ({tasks, OnRemoveLabelToTask,OnSelectTask }) =>
-{
-    return(<>
-    
-   
-    <Stack 
-    direction='column'
-    divider={<Divider orientation="horizontal" flexItem />}
-    style={{ overflowY:'auto', textAlign:'left', width:'99%', height:'100%',border:'1px', solid:'#ccc',margin:'10px', marginBottom:'50px' }}>
-                                 
-            <List component="nav" color='grey'>
-                {tasks.map((task) => (
-                    <>
-                        <Box key={task.toString()} sx={{width:'100%', maxWidth:'100%', bgColor:'grey', flexDirection:'row'}}>
-                            <ListItemButton onClick={(e) => OnSelectTask(task)}>
-                                <Box sx={{my:1, mx:2, width:'100%'}}>
-                                    <Grid container alignItems="center">
-                                        <Grid item xs={100}>
-                                            <Stack direction='row' alignItems='flex-start'>
-                                                <ClassOutlinedIcon sx={{fontSize:'40px'}} />
-                                                <Typography variant='h4' component="div">
-                                                    {task.title}
-                                                    <IconButton aria-label="Add">
-                                                        <HighlightOffIcon sx={{fontSize:'30px'}} />
-                                                    </IconButton>
-                                                </Typography>
-                                            </Stack>
-                                        </Grid>
-                                        <Grid item>
-                                            <Typography variant='h5' component="div">
-                                                {task.executionDate}
-                                            </Typography>
-                                        </Grid>
-                                    </Grid>
-                                             
-                                </Box>
-                            </ListItemButton>
-                                <Divider variant='middle'/>
-                                <Box sx={{width:'75%', maxWidth:'75%', bgColor:'grey', flexDirection:'row'}}>
-                                    <Stack component='nav' direction='row' flexWrap='wrap' spacing={0} margin={1}>
-                                        {task.labels.map((label) =>(
-                                            <>
-                                                <Chip key={label.toString()} style={{margin:5, padding:10}} size='medium' onDelete={OnRemoveLabelToTask} color='primary' label={label.title} />
-                                            </>
-                                        ))} 
-                                        <IconButton aria-label="Add">
-                                            <AddCircleIcon sx={{fontSize:'35px'}} color='primary'  />
-                                        </IconButton>
-                                     
-                                          
-                                    </Stack>            
-                                </Box>
-                            </Box>
-                     
-                        <Divider/>
-                    </>
-                ))
+const TasksItemGrid = ({ tasks, OnRemoveLabelToTask, OnSelectTask }) => {
+  return (
+    <>
+      <div
+        direction="column"
+        justifyContent="center"
+        style={{
+          overflow: "auto",
+          height: "100%",
+        }}
+      >
+        {tasks.map((task) => (
+          <>
+            {/* <Box
+                key={task.toString()}
+                sx={{
+                  width: "100%",
+                  maxWidth: "100%",
+                  bgColor: "grey",
+                  flexDirection: "row",
+                }}
+              >
+                <ListItemButton onClick={(e) => OnSelectTask(task)}>
+                  <Box sx={{ my: 1, mx: 2, width: "100%" }}>
+                    <Grid container alignItems="center">
+                      <Grid item xs={100}>
+                        <Stack direction="row" alignItems="flex-start">
+                          <ClassOutlinedIcon sx={{ fontSize: "40px" }} />
+                          <Typography variant="h4" component="div">
+                            {task.title}
+                            <IconButton aria-label="Add">
+                              <HighlightOffIcon sx={{ fontSize: "30px" }} />
+                            </IconButton>
+                          </Typography>
+                        </Stack>
+                      </Grid>
+                      <Grid item>
+                        <Typography variant="h5" component="div">
+                          {task.executionDate}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </Box>
+                </ListItemButton>
+
+                <Box
+                  sx={{
+                    width: "75%",
+                    maxWidth: "75%",
+                    bgColor: "grey",
+                    flexDirection: "row",
+                  }}
+                >
+                  <Stack
+                    component="nav"
+                    direction="row"
+                    flexWrap="wrap"
+                    spacing={0}
+                    margin={1}
+                  >
+                    {task.labels.map((label) => (
+                      <>
+                        <Chip
+                          key={label.toString()}
+                          style={{ margin: 5, padding: 10 }}
+                          size="medium"
+                          onDelete={OnRemoveLabelToTask}
+                          color="primary"
+                          label={label.title}
+                        />
+                      </>
+                    ))}
+                  </Stack>
+                </Box>
+              </Box> */}
+
+            <Card key={task.toString()} sx={{ width: "90%", marginBottom:5, marginTop:5, marginLeft:"5%"}}>
+              <CardHeader
+                action={
+                  <IconButton aria-label="Add">
+                    <HighlightOffIcon sx={{ fontSize: "30px" }} />
+                  </IconButton>
                 }
-            </List>
-     </Stack>
-     </>)
-}
+                title={task.title}
+                subheader={task.executionDate}
+              />
+              <CardContent>
+                {task.labels.map((label) => (
+                  <>
+                    <Chip
+                      key={label.toString()}
+                      style={{ margin: 5}}
+                      size="medium"
+                      onDelete={OnRemoveLabelToTask}
+                      onClick={OnRemoveLabelToTask}
+                      color="primary"
+                      label={label.title}
+                    />
+                  </>
+                ))}
+              </CardContent>
+            </Card>
+          </>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default TasksItemGrid;

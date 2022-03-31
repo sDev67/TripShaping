09e82fedf1d2d8ff014e736010bcc6fc2ef45615
GameWorldStudio <enzomazzarella1@gmail.com>
2022-03-31 21:26:24 +0200

    Mise en place de la route login + debut du form

--- Developpement/Web/web_project/src/routes/Login.js
+++ Developpement/Web/web_project/src/routes/Login.js
@@ -1,11 +1,95 @@
 import { Stack } from "@mui/material";
-
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 const Login = () => 
 {
+    const [values, setValues] = React.useState({
+        amount: '',
+        password: '',
+        mail: '',
+        weight: '',
+        weightRange: '',
+        showPassword: false,
+      });
+    
+      const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+      };
+    
+      const handleClickShowPassword = () => {
+        setValues({
+          ...values,
+          showPassword: !values.showPassword,
+        });
+      };
+    
+      const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+      };
+
+
     return(
     <>
-        <Stack>
-            None content
+        <Stack alignItems='center'>
+            <Typography
+            color="primary"
+            variant="h2"
+            textAlign="center"
+            marginTop={4}
+            >
+            Login
+            </Typography>
+
+            <Stack marginTop='15vh'>
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Mail address</InputLabel>
+                    <OutlinedInput
+
+                        type={'email'}
+                        value={values.mail}
+                        onChange={handleChange('mail')}
+
+                        label="Mail Address"
+                    />
+                </FormControl>
+
+                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={values.showPassword ? 'text' : 'password'}
+                        value={values.password}
+                        onChange={handleChange('password')}
+                        endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                            >
+                            {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                        }
+                        label="Password"
+                    />
+                </FormControl>
+            </Stack>
+            
+           
         </Stack>
             
        

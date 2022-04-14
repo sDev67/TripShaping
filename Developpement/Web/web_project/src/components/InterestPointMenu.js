be3084570709360aa8448b5117ae7a2b24a1c3d9
Baptiste Flejou <baptiste.flejou@gmail.com>
2022-04-14 18:43:58 +0200

    liaison point d'interet et etape

--- Developpement/Web/web_project/src/components/InterestPointMenu.js
+++ Developpement/Web/web_project/src/components/InterestPointMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Card,
   TextField,
@@ -16,14 +16,17 @@ import DoneRounded from "@mui/icons-material/DoneRounded";
 import UploadFileRounded from "@mui/icons-material/UploadFileRounded";
 import CancelRounded from "@mui/icons-material/CancelRounded";
 import { FileUploader } from "react-drag-drop-files";
-import { useQueryClient } from 'react-query';
+import { useQueryClient, useQuery } from 'react-query';
 import RichTextEditor from "./RichTextEditor";
+import StepRequests from "../requests/StepRequests";
+
 const InterestPointMenu = ({
   deletePoint,
   selectedMarker,
   setSelectedMarker,
   updateInfoPoint,
-  isEdition
+  isEdition,
+  steps
 }) => {
   const queryClient = useQueryClient();
 
@@ -31,8 +34,12 @@ const InterestPointMenu = ({
   const [title, setTitle] = useState(selectedMarker.title);
   const [category, setCategory] = useState(selectedMarker.category);
   const [description, setDescription] = useState(selectedMarker.description);
-  const [lengthOfStay, setLengthOfStay] = useState(selectedMarker.duration);
   const [dialogOpen, setDialogOpen] = useState(false);
+  const [stepId, setStepId] = useState(selectedMarker.StepId);
+  const [selectedStep, setSelectedStep] = useState(null);
+  const [days, setDays] = useState([]);
+  const [dayStep, setDayStep] = useState(selectedMarker.day);
+  const listSteps = steps;
 
   const categ = [
     {
@@ -68,28 +75,56 @@ const InterestPointMenu = ({
     {
       value: 'Autre',
     },
-    
-    
   ];
 
- // Fonction qui met a jour les propriétés d'un point d'interet
+  const fillDays = () => {
+    if (selectedStep) {
+      setDays([]);
+      for (let i = 0; i < selectedStep.duration; i++) {
+        let day = { date: "Jour " + (i + 1), value: i + 1 }
+        setDays(days => [...days, day]);
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (stepId) {
+      if (listSteps) {
+        listSteps.forEach(step => {
+          if (step.id === stepId) {
+            setSelectedStep(step)
+          }
+        });
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (selectedStep) {
+      fillDays()
+    }
+  }, [selectedStep]);
+
+  // Fonction qui met a jour les propriétés d'un point d'interet
   const updateInterestPointInfo = (pointId) => (e) => {
-    if(isEdition){
+    if (isEdition) {
       const newPoint = {
         title: title,
         category: category,
         description: description,
-        idPoint: pointId.id
+        idPoint: pointId.id,
+        StepId: selectedStep?.id,
+        day: dayStep
       };
       updateInfoPoint.mutate(newPoint);
       setSelectedMarker(null);
-    }  
+    }
   };
-  
+
 
   return (
     <>
-      <Card style={{ right: "3%", top: "5%", width: 400, position: "fixed", height:"90%" }}>
+      <Card style={{ right: "3%", top: "5%", width: 400, position: "fixed", height: "90%" }}>
         <CardMedia
           component="img"
           height="194"
@@ -103,17 +138,16 @@ const InterestPointMenu = ({
           <CancelRounded />
         </IconButton>
 
-        <CardContent 
-        style={{
-  
-          overflowY: "auto",
-          height:'100%'
-        }}>
+        <CardContent
+          style={{
+            overflowY: "auto",
+            height: '100%'
+          }}>
           <TextField
             fullWidth
             label="Nom"
             value={title}
-            onChange={(e) =>setTitle(e.target.value)}
+            onChange={(e) => setTitle(e.target.value)}
             style={{ marginBottom: 25 }}
             InputLabelProps={{
               shrink: true,
@@ -132,13 +166,12 @@ const InterestPointMenu = ({
               shrink: true,
             }}
             disabled={!isEdition}
-
           >
             {categ.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.value}
-            </MenuItem>
-          ))}
+              <MenuItem key={option.value} value={option.value}>
+                {option.value}
+              </MenuItem>
+            ))}
           </TextField>
           <Stack
             style={{ marginBottom: 25 }}
@@ -150,28 +183,39 @@ const InterestPointMenu = ({
               fullWidth
               select
               label="Etape associée"
-              
+              value={selectedStep}
+              onChange={(e) => setSelectedStep(e.target.value)}
+
               InputLabelProps={{
                 shrink: true,
               }}
+              disabled={!isEdition}
             >
-              <MenuItem>Etape 1</MenuItem>
-              <MenuItem>Etape 2</MenuItem>
+              {listSteps &&
+                listSteps.map((step, index) => (
+                  <MenuItem key={index} value={step}>{step.title}</MenuItem>
+                ))
+              }
             </TextField>
             <TextField
               fullWidth
               select
-              label="Jour"
-              
+              label="Jour associé"
+              value={dayStep}
+              onChange={(e) => setDayStep(e.target.value)}
+
               InputLabelProps={{
                 shrink: true,
               }}
+              disabled={isEdition && selectedStep ? false : true}
             >
-              <MenuItem>1</MenuItem>
-              <MenuItem>2</MenuItem>
+              {days.map((day, index) => (
+                <MenuItem key={index} value={day.value}>{day.date}</MenuItem>
+              ))
+              }
             </TextField>
           </Stack>
-          
+
           <Stack
             style={{ marginBottom: 25 }}
             direction="row"
@@ -220,9 +264,9 @@ const InterestPointMenu = ({
 
           />*/}
 
-            <RichTextEditor setValue={setDescription} value={description} limitedEditor={true} minH='10px' isReadOnly={!isEdition}/>
+          <RichTextEditor setValue={setDescription} value={description} limitedEditor={true} minH='10px' isReadOnly={!isEdition} />
 
-         
+
           <Stack direction="row" justifyContent="space-between">
             {isEdition && <>
               <Button
@@ -232,21 +276,18 @@ const InterestPointMenu = ({
                 onClick={() => { deletePoint.mutate(selectedMarker.id); setSelectedMarker(null) }}
               >
                 Supprimer
-              </Button>            
-            
+              </Button>
+
               <Button
                 variant="contained"
                 color="primary"
                 startIcon={<DoneRounded />}
-                //onClick={updateProperties(selectedMarker)}
                 onClick={updateInterestPointInfo(selectedMarker)}
-
               >
                 Enregistrer
               </Button>
-              </>
+            </>
             }
-            
           </Stack>
         </CardContent>
       </Card>
--- Developpement/Web/web_project/src/components/Map.js
+++ Developpement/Web/web_project/src/components/Map.js
@@ -32,7 +32,7 @@ const containerStyle = {
   height: "100%",
 };
 
-export const Map = ({}) => {
+export const Map = ({ }) => {
   const queryClient = useQueryClient();
 
   let { idTravel } = useParams();
@@ -433,7 +433,7 @@ export const Map = ({}) => {
                 onDragEnd={updateStepLocation(step)}
                 icon={
                   selectedMarker?.marker.id == step.id &&
-                  selectedMarker?.type == "Step"
+                    selectedMarker?.type == "Step"
                     ? selectedStepIcon
                     : stepIcon
                 }
@@ -470,7 +470,7 @@ export const Map = ({}) => {
                 onDragEnd={updateInterestPointLocation(interestPoint)}
                 icon={
                   selectedMarker?.marker.id == interestPoint.id &&
-                  selectedMarker?.type == "Point"
+                    selectedMarker?.type == "Point"
                     ? selectedInterestPointIcon
                     : interestPointIcon
                 }
@@ -548,6 +548,7 @@ export const Map = ({}) => {
               deletePoint={deletePoint}
               updateInfoPoint={updateInfoPoint}
               isEdition={isEdition}
+              steps={steps}
             ></InterestPointMenu>
           )
         ) : selectedMarker.type === "Step" && isLoadingS ? (

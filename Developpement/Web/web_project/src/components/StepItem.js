8395398d1abbbf5c3baab30b1d6d17b18e6e94f8
Benjamin Gallier <bgallier@unistra.fr>
2022-05-09 18:54:56 +0200

    changements album

--- Developpement/Web/web_project/src/components/StepItem.js
+++ Developpement/Web/web_project/src/components/StepItem.js
@@ -7,11 +7,14 @@ import {
   Accordion,
   Typography,
   Divider,
-  Box,
+  CardHeader,
   TextField,
   MenuItem,
   Button,
   InputAdornment,
+  Card,
+  Grid,
+  CardContent,
 } from "@mui/material";
 
 import LocationOnRounded from "@mui/icons-material/LocationOnRounded";
@@ -25,6 +28,7 @@ import { useQuery, useQueryClient, useMutation } from "react-query";
 import { useParams } from "react-router-dom";
 import Loading from "../utils/Loading";
 import DocumentsList from "./DocumentsList";
+import StepRequests from "../requests/StepRequests";
 
 const StepItem = ({ step, index, updateInfoStep }) => {
   const queryClient = useQueryClient();
@@ -63,6 +67,17 @@ const StepItem = ({ step, index, updateInfoStep }) => {
   } = useQuery(["getDocumentsOfStep", step.id], () =>
     DocumentRequest.getDocumentsByStepId(step.id)
   );
+
+  const {
+    isLoading: isLoadingP,
+    isError: isErrorP,
+    error: errorP,
+    data: points,
+  } = useQuery(["getPointsOfStep", step.id], () =>
+    StepRequests.getPointsOfStep(step.id)
+  );
+
+  console.log(points);
 
   const addFile = (file) => {
     const formData = new FormData();
@@ -119,9 +134,15 @@ const StepItem = ({ step, index, updateInfoStep }) => {
             <InsertDriveFileRoundedIcon color="primary"></InsertDriveFileRoundedIcon>
           </Stack>
           <Stack direction="row" alignItems="center" spacing={1} width="5%">
-            <Typography variant="h5" style={{ fontWeight: "normal" }}>
-              1
-            </Typography>
+            {isLoadingP ? (
+              <Loading />
+            ) : isErrorP ? (
+              <p style={{ color: "red" }}>{errorP.message}</p>
+            ) : (
+              <Typography variant="h5" style={{ fontWeight: "normal" }}>
+                {points.length}
+              </Typography>
+            )}
             <LocationOnRounded color="error"></LocationOnRounded>
           </Stack>
         </Stack>
@@ -129,56 +150,69 @@ const StepItem = ({ step, index, updateInfoStep }) => {
       <AccordionDetails>
         <Divider></Divider>
         <Stack direction="column" spacing={5} marginTop={2}>
-          <Stack direction="row" spacing={2} alignItems="center">
-            <TextField
-              id={"title" + index}
-              fullWidth
-              label="Titre"
-              variant="outlined"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-            <TextField
-              id={"duration" + index}
-              label="Durée"
-              style={{ maxWidth: "10%" }}
-              variant="outlined"
-              value={duration}
-              onChange={(e) => setDuration(e.target.value)}
-              endAdornment={
-                <InputAdornment position="end">jours</InputAdornment>
-              }
-              InputLabelProps={{
-                shrink: true,
-              }}
-              InputProps={{
-                endAdornment: (
+          <div>
+            <Stack direction="row" justifyContent="flex-end" marginBottom={2}>
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<DoneRounded />}
+                onClick={updateStepInfo(step.id)}
+              >
+                Enregistrer
+              </Button>
+            </Stack>
+
+            <Stack direction="row" spacing={2} alignItems="center">
+              <TextField
+                id={"title" + index}
+                fullWidth
+                label="Titre"
+                variant="outlined"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <TextField
+                id={"duration" + index}
+                label="Durée"
+                style={{ maxWidth: "10%" }}
+                variant="outlined"
+                value={duration}
+                onChange={(e) => setDuration(e.target.value)}
+                endAdornment={
                   <InputAdornment position="end">jours</InputAdornment>
-                ),
-              }}
-            />
-            <TextField
-              id={"category" + index}
-              fullWidth
-              variant="outlined"
-              select
-              label="Catégorie"
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            >
-              {categ.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.value}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Stack>
+                }
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">jours</InputAdornment>
+                  ),
+                }}
+              />
+              <TextField
+                id={"category" + index}
+                fullWidth
+                variant="outlined"
+                select
+                label="Catégorie"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              >
+                {categ.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.value}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Stack>
+          </div>
 
           {/* <TextField
                 id={"description" + index}
@@ -238,14 +272,49 @@ const StepItem = ({ step, index, updateInfoStep }) => {
               ></DocumentsList>
             )}
           </Stack>
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<DoneRounded />}
-            onClick={updateStepInfo(step.id)}
-          >
-            Enregistrer
-          </Button>
+          {isLoadingP ? (
+            <Loading />
+          ) : isErrorP ? (
+            <p style={{ color: "red" }}>{errorP.message}</p>
+          ) : (
+            <div style={{ overflowY: "scroll", height: "200px" }}>
+              <Grid container spacing={5}>
+                {Array.from(Array(duration), (index, day) => {
+                  return (
+                    <Grid key={index} item xs={3}>
+                      <Card>
+                        <CardHeader
+                          title={
+                            <Typography variant="h5" color="primary">
+                              Jour {day + 1}
+                            </Typography>
+                          }
+                        ></CardHeader>
+                        <CardContent>
+                          {points.map(
+                            (point) =>
+                              point.day === day + 1 && (
+                                <Stack
+                                  direction="row"
+                                  justifyContent="space-between"
+                                >
+                                  <Typography variant="h7">
+                                    • {point.title}
+                                  </Typography>
+                                  <Typography variant="body2">
+                                    {point.category}
+                                  </Typography>
+                                </Stack>
+                              )
+                          )}
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </div>
+          )}
         </Stack>
       </AccordionDetails>
     </Accordion>
